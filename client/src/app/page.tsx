651ccd4db9b8a903400aefac96276a0c29b62985
Waquar Ahmad <waquar.ahmad4321@gmail.com>
2025-03-09 19:07:52 +0530

    `Added the landing page`

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,13 @@
 import NonDashboardNavbar from "@/components/NonDashboardNavbar";
+import Landing from "@/app/(nondashboard)/landing/page";
 
 export default function Home() {
   return (
     <div className="nondashboard-layout">
         <NonDashboardNavbar />
+        <main className="nondashboard-layout__main">
+          <Landing />
+        </main>
     </div>
   );
 }
