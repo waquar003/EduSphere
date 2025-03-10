55e5711a278e78f60cce48f3dc6c4ab710f6033f
Waquar Ahmad <waquar.ahmad4321@gmail.com>
2025-03-10 12:44:41 +0530

    `feat: implement RTK Query API setup with course types and endpoints`

---
+++ client/src/state/api.ts
@@ -0,0 +1,48 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BaseQueryApi, BaseQueryFn, FetchArgs } from "@reduxjs/toolkit/query";
+
+const customBaseQuery = async (
+    args: string | FetchArgs,
+    api: BaseQueryApi,
+    extraOptions: any
+) => {
+    const baseQuery = fetchBaseQuery({
+        baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+    });
+
+    try {
+        const result: any = await baseQuery(args, api, extraOptions);
+
+        if(result.data) {
+            return { data: result.data };
+        }
+    } catch (error: unknown) {
+        const errorMessage = error instanceof Error ? error.message : "Unknown error";
+    
+        return { error: { status: "FETCH_ERROR", error: errorMessage } };
+    }
+}
+
+export const api = createApi({
+    baseQuery: customBaseQuery as BaseQueryFn,
+    reducerPath: "api",
+    tagTypes: ["Courses"],
+    endpoints: (build) => ({
+        getCourses: build.query<Course[], { category?: string }>({
+            query: ({ category }) => ({
+                url: "courses",
+                params: { category },
+            }),
+            providesTags: ["Courses"],
+        }),
+        getCourse: build.query<Course, string>({
+            query: (id) => `course/${id}`,
+            providesTags: (result, error, id) => [{ type: "Courses", id }],
+        }),
+        
+    }),
+})
+export const {
+    useGetCoursesQuery,
+    useGetCourseQuery,
+} = api;
---
+++ client/src/types/index.d.ts
@@ -0,0 +1,37 @@
+declare global {
+    interface Course {
+        courseId: string;
+        teacherId: string;
+        teacherName: string;
+        title: string;
+        description?: string;
+        category: string;
+        image?: string;
+        price?: number; // Stored in cents (e.g., 4999 for $49.99)
+        level: "Beginner" | "Intermediate" | "Advanced";
+        status: "Draft" | "Published";
+        sections: Section[];
+        enrollments?: Array<{
+          userId: string;
+        }>;
+    }
+
+    interface Section {
+        sectionId: string;
+        sectionTitle: string;
+        sectionDescription?: string;
+        chapters: Chapter[];
+    }
+
+    interface Chapter {
+        chapterId: string;
+        title: string;
+        content: string;
+        video?: string | File;
+        freePreview?: boolean;
+        type: "Text" | "Quiz" | "Video";
+    }
+}
+
+
+export {};
